bf42947cfe6392df5ad83896c2af3d4c309ba362
Sebastain R <supasebie@gmail.com>
2020-09-18 16:05:39 -0700

    Missed file add on previous commit

--- App-SPA/src/app/app.module.ts
+++ App-SPA/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { ErrorInterceptorProvider } from './_services/error.interceptor';
 import { AuthService } from './_services/auth.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -22,7 +23,8 @@ import { HomeComponent } from './home/home.component';
     FormsModule
   ],
   providers: [
-    AuthService
+    AuthService,
+    ErrorInterceptorProvider
   ],
   bootstrap: [AppComponent]
 })
